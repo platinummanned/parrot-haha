997302b1be2532e63199c6e7066f91153a04e7b3
platinummanned <126128454+platinummanned@users.noreply.github.com>
2023-11-16 17:05:02 -0800

    Add files via upload

--- parrot.py
+++ parrot.py
@@ -1,11 +1,14 @@
 from discord.ext import commands
 import discord
-import random
 
-msgList = ["five dollar fake dollar"]
+import random
+import responses
+
+#the abundacne of lists
+msgList = [" "]
 bannedWords = ["nigger", "nigga", ">send", ">help", ">repeat"]
 noises = ["\*squawk*", "\*chirp*", "\*caw*", "\*tweet*", "\*caw caw*"]
-        
+
 bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
 
 def addToList(msg):
@@ -30,31 +33,36 @@ def getMessage():
 @bot.event
 async def on_ready():
     print("bot olnine")
-    channel = bot.get_channel(1161098199260471306) #testing channel
+    channel = bot.get_channel(1161098199260471306)
     await channel.send(random.choice(noises))
 
 @bot.event
 async def on_message(msg):
     if msg.author == bot.user:
         return
-    
-    addToList(msg.content)
 
-    if random.randint(1, 20) == 1:
+    if bot.user.mentioned_in(msg):
+        await msg.reply(msg.author.mention + " " + responses.handlePing(msg))
+    elif random.randint(1, 20) == 1:
         await msg.channel.send(getMessage())
+        addToList(msg.content)
     elif random.randint(1, 100) == 1:
         await msg.channel.send("$100 bills")
+        addToList(msg.content)
+    else:
+        addToList(msg.content)
 
     await bot.process_commands(msg)
 
 @bot.command()
 async def send(ctx):
-    if ctx.message.author.id == 804463117232242748: #decided to make this command owner only
+    if ctx.message.author.id == 804463117232242748:
         await ctx.send(getMessage())
 
 @bot.command()
 async def repeat(ctx, *msg):
-    await ctx.send(" ".join(msg))
+    if ctx.message.author.id == 804463117232242748:
+        await ctx.send(" ".join(msg))
 
-
-bot.run(token="INSERT TOKEN HERE")
+#no token for you :)
+bot.run(token="")
---
+++ responses.py
@@ -0,0 +1,76 @@
+from discord.ext import commands
+import discord
+
+import random
+
+a = False
+
+#bird
+noises = ["\*squawk*", "\*chirp*", "\*caw*", "\*tweet*", "\*caw caw*"]
+
+#on ping
+greetings = ["hihihi!!!", "hi!!!!!", "hiya", "SHUT UP", "https://media.discordapp.net/attachments/832893472356302868/1091790331437121648/piong_again_n_sex.gif", "im gonna jack ur box"]
+positive = ["YAY UR ONLINE", "I LOVE U", "U THE GOAT FR", "this guy is a massive W", "sex pls"]
+negative = ["fuck you man", "i hate you", "this guy sucks", "i dont like you bro"]
+
+#on question
+questions = ["why u ping!?!?!?", "wut u want?", "what u need???", "u has question?"]
+yesno = ["yes", "yeah", "yup", "yuh uh", "no", "nope", "nah", "nuh uh"]
+rather = ["option 1", "#1 for sure", "first one seems good", "ill take #1", "option 2", "#2 on top", "second one sounds nice", "ill go for #2"]
+howto = ["idk man dont ask me", "im just a parrot i cant do this shit", ":man_shrugging:", "you have to like do yknow", "open the jar then take out the cookies and crush it into your face", "you have to jack your box"]
+
+#on ping + msg
+accept = ["oh ok", "okie dokie", "ohhhhh", "alr got it"]
+decline = ["i dont think so", "are u sure", "idk man", "hmm..."]
+
+
+def getResponse(t):
+    return random.choice(t)
+
+def findLiked(i):
+    with open(r"C:\Users\keshi\Documents\thing\Python\parrot\loved.txt", "r") as file:
+        content = file.read()
+
+        if str(i) in content:
+            return True
+        else:
+            return False
+
+def findHated(i):
+    with open(r"C:\Users\keshi\Documents\thing\Python\parrot\hated.txt", "r") as file:
+        content = file.read()
+
+        if str(i) in content:
+            return True
+        else:
+            return False
+
+
+def handlePing(ctx):
+    global a
+
+    msg = ctx.content
+    i = ctx.author.id
+
+    #if findLiked(i) and not findHated(i):
+    #    return getResponse(positive)
+    #elif findHated(i) and not findLiked(i):
+    #    return getResponse(negative)
+
+    if msg == "<@1161070845934960900>":
+        return getResponse(greetings)
+    elif msg.find("?") != -1 and msg.find(" or ") == -1 and msg.find("how ") == -1:
+        return getResponse(yesno)
+    elif msg.find(" or ") != -1 and msg.find("how ") == -1:
+        return getResponse(rather)
+    elif msg.find(" or ") == -1 and msg.find("how ") != -1:
+        return getResponse(howto)
+    elif msg.find("?") == -1 and msg.find(" or ") == -1 and msg.find("how ") == -1:
+        if a == False:
+            a = True
+            return getResponse(questions)
+        elif a == True:
+           a = False
+           return getResponse(accept) 
+    else:
+        return "idk wut ur tryna tell me rn"
